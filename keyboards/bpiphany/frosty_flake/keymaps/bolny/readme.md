9d078ac68f9196782212f4ebd4358e26b3f985a1
Brendon O'Laney <bolny@Goodsprings.local>
2024-11-26 16:00:49 -0800

    re-add keymaps

---
+++ keyboards/bpiphany/frosty_flake/keymaps/bolny/readme.md
@@ -0,0 +1,3 @@
+1. Enable the keyboard's native media keys.
+2. Add a virtual numpad.
+3. Add a layer to enable the caps lock key.
---
+++ keyboards/ploopyco/trackball/keymaps/bolny/keymap.c
@@ -0,0 +1,60 @@
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ * Copyright 2019 Sunjun Kim
+ * Copyright 2020 Ploopy Corporation
+ * Copyright 2022 Brendon O'Laney (@bolny) <brendon@brendonolaney.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum brendon_keycodes {
+    COPY = PLOOPY_SAFE_RANGE,
+    PASTE,
+    MCTRL,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT( /* Base */
+        KC_BTN1, KC_BTN3, KC_BTN2,
+          MCTRL, OSL(1)
+    ),
+    [1] = LAYOUT(
+        COPY, _______, PASTE,
+          DRAG_SCROLL, MO(0)
+    )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch(keycode) {
+        case COPY:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("c"));
+            }
+            return false;
+            break;
+        case PASTE:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("v"));
+            }
+            return false;
+            break;
+        case MCTRL:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL(SS_TAP(X_UP)));
+            }
+            return false;
+            break;
+    }
+    return true;
+}
